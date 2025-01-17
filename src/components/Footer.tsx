9db72c92621db4d1aff87066ba963e0e5723021e
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2025-01-16 20:46:56 -0600

    Create static page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between w-[972px] items-center text-[11px] opacity-30 mt-[12px]">
+      <small className="text-[11px]">&copy; 2089. Copyright by Samarth</small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,25 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = [
+  "buy groceries",
+  "walk the dog",
+  "do laundry",
+  "study for exam",
+];
+
+export default function TodoList() {
+  return (
+    <ul>
+      {initialTodos.map((todo, index) => (
+        <li
+          key={index}
+          className="flex justify-between items-center px-8 
+          h-[50px] text-[14px] cursor-pointer border-b 
+        border-black/[8%]"
+        >
+          <span>{todo}</span> <DeleteButton />
+        </li>
+      ))}
+    </ul>
+  );
+}
